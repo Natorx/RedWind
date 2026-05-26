6b4dcd5e89e4eeed62473556283e1172428d5a38
Fofow <1760855795@qq.com>
2026-05-26 19:18:43 +0800

    version up

--- client/pages/DrillGround.tsx
+++ client/pages/DrillGround.tsx
@@ -1,7 +1,5 @@
-import { invoke } from "@tauri-apps/api/core";
+
 const DrillGround: React.FC = () => {
-  const read_doc = invoke('read_document');
-  const export_doc = invoke('export_markdown');
   return (
     <div>
       
